60c2812dae611a60fd1f8b4e89c19d022db3d766
Corey Koontz <corey.michael.koontz@gmail.com>
2021-05-01 21:54:25 -0500

    Working on post exercise

---
+++ src/main/java/com/codeup/SpringBlog/Post.java
@@ -0,0 +1,32 @@
+package com.codeup.SpringBlog;
+
+public class Post {
+
+    private long id;
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
