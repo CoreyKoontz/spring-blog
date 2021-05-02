f0b18f4f11ccc64e4e9eead730cd93c3f2db5186
Corey Koontz <corey.michael.koontz@gmail.com>
2021-05-02 01:36:47 -0500

    Broke the post/index need to fix

--- src/main/java/com/codeup/SpringBlog/controllers/PostController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/PostController.java
@@ -1,21 +1,31 @@
 package com.codeup.SpringBlog.controllers;
 
+import com.codeup.SpringBlog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
     @GetMapping("/post")
-    @ResponseBody
-    public String post() {
-        return "post index page";
+    public String post(Model model) {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1, "title1", "body1"));
+        posts.add(new Post(2, "title2", "body2"));
+        posts.add(new Post(3, "title3", "body3"));
+        model.addAttribute("posts", posts);
+        return "post/index";
     }
 
-    @GetMapping("/post/{id}")
-    @ResponseBody
-    public String viewPost(@PathVariable int id) {
-        return "View post with id: " + id;
+    @GetMapping("/show")
+    public String viewPost(@PathVariable int id, Model model) {
+        Post post = (Post) model.getAttribute("post-link");
+        model.addAttribute("post", post);
+        return "post/show";
     }
 
     @GetMapping("/post/create")
