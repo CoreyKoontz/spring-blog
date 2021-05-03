d7321bc01880eaf91871ff1e8c33d9f6bfb4ebd6
Corey Koontz <corey.michael.koontz@gmail.com>
2021-05-03 16:45:05 -0500

    Complete mini-exercise with dog entity

--- src/main/java/com/codeup/SpringBlog/models/Dog.java
+++ src/main/java/com/codeup/SpringBlog/models/Dog.java
@@ -3,12 +3,12 @@ package com.codeup.SpringBlog.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "dogs", uniqueConstraints = @UniqueConstraint(columnNames = {"name", "resideState"}))
+@Table(name = "dogs", uniqueConstraints = @UniqueConstraint(name = "UK_name_AND_reside_state", columnNames = {"name", "resideState"}))
 public class Dog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "UNSIGNED")
+    @Column(columnDefinition = "INT UNSIGNED")
     private long id; // Can we add a length to int? or tinyint?
 
     @Column(columnDefinition = "TINYINT(3) UNSIGNED NOT NULL ")
@@ -19,6 +19,9 @@ public class Dog {
 
     @Column(columnDefinition = "CHAR(2) DEFAULT 'XX'")
     private String resideState;
+
+    public Dog() {
+    }
 
     public Dog(long id, int age, String name, String resideState) {
         this.id = id;
